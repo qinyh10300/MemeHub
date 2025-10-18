fdf09700837a0af15be09e9901bf718a71ee6071
Qin Yihua <qinyh10300@gmail.com>
2025-10-18 15:50:04 +0800

    feat: 登录 注册 router

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -9,10 +9,12 @@ import HelloWorld from './components/HelloWorld.vue'
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
+      <!-- <HelloWorld msg="秦一骅" /> -->
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/login">Login</RouterLink>
       </nav>
     </div>
   </header>
