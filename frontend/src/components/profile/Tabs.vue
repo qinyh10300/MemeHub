c787d1afdc82c773f6178304050fc81e5b361fcd
Qin Yihua <qinyh10300@gmail.com>
2025-11-27 19:05:42 +0800

    fix：个人页面模因跳转

--- frontend/src/components/profile/Tabs.vue
+++ frontend/src/components/profile/Tabs.vue
@@ -45,7 +45,7 @@
         >
         <button
           class="meme-item"
-          :disabled="meme.status === 'banned'"
+          :disabled="meme.status === 'banned' || meme.status === 'pending'"
           @click="goToMemeDetail(meme.id)"
         >
           <img :src="meme.image" alt="meme" class="meme-image" />
